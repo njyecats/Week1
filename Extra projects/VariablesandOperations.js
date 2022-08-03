56df25adc8a59d6d15b3d7a6dbbf8e4ff3880c15
Stacey Null <njyecats@yahoo.com>
2022-08-03 12:14:19 -0700

    Added variables

--- Extra projects/VariablesandOperations.js
+++ Extra projects/VariablesandOperations.js
@@ -18,24 +18,33 @@ let customerFirstName = "Sally";
 let streetAddress = "1234 W Easy St";
 
 //print all variables to the console
-console.log("Sally at 1234 W Easy Dr", typeof middleInitial);
-
+console.log(availablePlaneSeats);
+console.log (costOfGroceries);
+console.log (middleInitial);
+console.log (isHotOutside);
+console.log (customerFirstName);
+console.log (streetAddress);
 
 
 //a customer books 2 plane seats, remove 2 seats from the available seats variable
-
+availablePlaneSeats = availablePlaneSeats -2;
+console.log(availablePlaneSeats);
 
 //impulse candy bar purchase, add 2.15 to the grocery total
-//costOfGroceries = costOfGroceries + 2.15;
+costOfGroceries = costOfGroceries + 2.15;
+console.log (costOfGroceries);
 
-
-//birth certificate was printed incorrectly, change the middle initial to sometehing else
-
+//birth certificate was printed incorrectly, change the middle initial to something else
+middleInitial = 'S';
+console.log (middleInitial);
 
 //the season has changed, update the hot outside variable to be opposite of what it was
-
+isHotOutside = !isHotOutside;  
+console.log(isHotOutside);
 
 //create a new variable called full name using the customer's first name, the middle initial, and last name of your choice
-
+let fullName = customerFirstName +" "+ middleInitial + " Smith";
+console.log (fullName); 
 
 // print a line to the console that introduces the customer and says they live at the address variable
+console.log ("Hi, my name is " + customerFirstName + " and I live at " + streetAddress + ".");
