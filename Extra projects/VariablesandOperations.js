9c47f34486b42c73538a943a2326dc04d8c8761d
Stacey Null <njyecats@yahoo.com>
2022-08-03 11:30:59 -0700

    extra project

---
+++ Extra projects/VariablesandOperations.js
@@ -0,0 +1,41 @@
+//create a variable to hold the quantity of available plane seats left on a flight
+let availablePlaneSeats = 6;
+
+
+//create a variable to hold the cost of groceries at checkout
+let costOfGroceries = 23.64;
+
+//create a variable to hold a person's middle initial
+let middleInitial = 'J';
+
+//create a varible to hold true if it's hot outside and false if it's cold outside
+let isHotOutside = false;
+
+//create a variable to hold a customer's first name
+let customerFirstName = "Sally";
+
+//create a variable to hold a street address
+let streetAddress = "1234 W Easy St";
+
+//print all variables to the console
+console.log(availablePlaneSeats);
+
+
+
+//a customer books 2 plane seats, remove 2 seats from the available seats variable
+
+
+//impulse candy bar purchase, add 2.15 to the grocery total
+//costOfGroceries = costOfGroceries + 2.15;
+
+
+//birth certificate was printed incorrectly, change the middle initial to sometehing else
+
+
+//the season has changed, update the hot outside variable to be opposite of what it was
+
+
+//create a new variable called full name using the customer's first name, the middle initial, and last name of your choice
+
+
+// print a line to the console that introduces the customer and says they live at the address variable
